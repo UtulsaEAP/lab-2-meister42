97cfc9b41042199b68fb53340b77633c5bfa815a
meister42 <hgh585@utulsa.edu>
2024-02-01 14:10:54 -0600

    problem 1

--- Problem 1/right_arrow.py
+++ Problem 1/right_arrow.py
@@ -2,10 +2,13 @@ def right_arrow():
     base_char = input()
     head_char = input()
 
-    row1 = '      ' + head_char
+    row0 = "      " + head_char
+    row1 = base_char + base_char + base_char + base_char + base_char + base_char + head_char
+    row2 = base_char + base_char + base_char + base_char + base_char + base_char + head_char + head_char
+    row3 = base_char + base_char + base_char + base_char + base_char + base_char + head_char + head_char + head_char
     ''' Type your code here. '''
 
-    print(row1)
+    print(row0)
     print(row2)
     print(row3)
     print(row2)
