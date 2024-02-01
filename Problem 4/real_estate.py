d77f2b8b3e5bb780b19cdfe6b1a4cecce4972d13
meister42 <hgh585@utulsa.edu>
2024-02-01 14:59:55 -0600

    problem 5

--- Problem 4/real_estate.py
+++ Problem 4/real_estate.py
@@ -1,8 +1,25 @@
+"""
 
+This house is $200000. The change is $-10000 since last month.
+    The estimated monthly mortgage is $850.00.
+This house is $200000. The change is $-10000 since last month.
+    The estimated monthly mortgage is $850.00.
+
+"""
 def real_estate():
     
     current_price = int(input())
     last_month_price = int(input())
     # Your code goes here
+
+    change = current_price - last_month_price
+    mortgage = (current_price * 0.051) / 12
+    
+
+    print("This house is $" + str(current_price) + ". The change is $" + str(change) + " since last month.\nThe estimated monthly mortgage is $" + f'{mortgage:.2f}' + ".")
+
+
+
+
 if __name__ == "__main__":
     real_estate()
--- Problem 5/simple_stats.py
+++ Problem 5/simple_stats.py
@@ -5,5 +5,16 @@ def simple_stats():
     num3 = float(input())
     num4 = float(input())
     ''' Type your code here. '''
+
+    avg1 = (num1 + num2 + num3 + num4)/4
+    prod1 = num1 * num2 * num3 * num4
+    avg2 = (num1 + num2 + num3 + num4)/4
+    prod2 = num1 * num2 * num3 * num4
+
+    #Output each rounded integer using the following:
+    print(f'{prod1:.0f}' + " " + f'{avg1:.0f}')
+
+    #Output each floating-point value with three digits after the decimal point, which can be achieved as follows:
+    print(f'{prod2:.3f}' + " " + f'{avg2:.3f}')
 if __name__ == "__main__":
     simple_stats()
